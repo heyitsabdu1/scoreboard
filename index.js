69e52d2da74f42b69e98b4f8de9d796f9b8371e3
heyitsabdu1 <arabid8@gmail.com>
2026-01-03 15:28:36 +0500

    commit initial files

---
+++ index.js
@@ -0,0 +1,49 @@
+hscore = 0
+ascore = 0
+let HomeScore = document.getElementById("homescore")
+let AwayScore = document.getElementById("awayscore")
+
+function add1 (){
+    
+  hscore = hscore + 1
+  HomeScore.textContent = hscore 
+}
+
+function add2 (){
+    
+  hscore = hscore + 2
+  HomeScore.textContent = hscore  
+}
+
+function add3 (){
+    
+  hscore = hscore + 3
+  HomeScore.textContent = hscore
+}
+
+// create functions for guest score
+
+function add1add (){
+    
+  ascore = ascore + 1
+  AwayScore.textContent = ascore 
+}
+
+function add2add (){
+    
+  ascore = ascore + 2
+  AwayScore.textContent = ascore 
+}
+
+function add3add (){
+    
+  ascore = ascore + 3
+  AwayScore.textContent = ascore 
+}
+
+
+
+
+// add doms 
+// recall counts 
+
